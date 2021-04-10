57d6032932b22a500cd30bb2c3bb509d9e8566f7
Bowen <bowen@jiuchet.com>
2021-04-10 16:17:34 +0800

    整理

--- examples/main.js
+++ examples/main.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from "./router";
 
-import jiuchet from '../packages/utils/jiuchet'
-import MyComponents from '../packages'
 
-Vue.prototype.$jiuchet = jiuchet;
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import jiuchet from '../packages/utils/jiuchet'
+import jcsoft from '../packages'
 
 Vue.use(ElementUI);
-Vue.use(MyComponents);
+Vue.prototype.$jiuchet = jiuchet;
+Vue.use(jcsoft);
 
 if (process.env.NODE_ENV === 'development') Vue.config.productionTip = true;
-
 
 new Vue({
     el: '#app',

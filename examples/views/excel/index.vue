35a4b6482472d1da1f5555f566b38537acf4f812
Bowen <bowen@jiuchet.com>
2021-05-05 17:06:15 +0800

    换机开发备份

---
+++ examples/views/excel/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="Excel">
+        <h1>Excel测试</h1>
+
+        <hr>
+
+        <div id="x-spreadsheet-demo"></div>
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'Test',
+    components: {},
+    data() {
+        return {}
+    },
+    created() {
+        window.VM = this;
+    }
+}
+</script>

57075f366b0b0085cfe79a993576b51ba2e246ab
Bowen <bowen@jiuchet.com>
2021-05-07 14:41:39 +0800

    完成 修改sheet表名的演示

--- examples/views/xspreadsheet/index.vue
+++ examples/views/xspreadsheet/index.vue
@@ -6,7 +6,34 @@
         <input type="file" @change="inputChage">
         <el-button @click="getData">console表格数据</el-button>
         <el-button @click="exportExcel">导出Excel</el-button>
-        <x-spreadsheet ref="xs" :on-change="onChange" @spreadsheet="getSpreadsheet"></x-spreadsheet>
+        <el-button @click="openRenameDialog">修改表名</el-button>
+
+        <x-spreadsheet ref="xs" @spreadsheet="getSpreadsheet"></x-spreadsheet>
+
+        <el-dialog title="修改表格名称" :visible.sync="renameDialog">
+
+            <el-form ref="form" :model="renameSheetForm" label-width="80px">
+
+                <el-form-item label="表单">
+                    <el-select v-model="renameSheetForm.ind" placeholder="请选择操作的表单" required="true">
+                        <div v-for="(item, ind) in xsData" :key="ind">
+                            <el-option :label="item.name" :value="ind"></el-option>
+                        </div>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="表格名称">
+                    <el-input v-model="renameSheetForm.name"></el-input>
+                </el-form-item>
+                <el-form-item>
+                    <el-button type="primary" @click="renameSubmit">修改</el-button>
+                    <el-button @click="renameDialog = false">取消</el-button>
+                </el-form-item>
+
+            </el-form>
+
+        </el-dialog>
+
+
     </div>
 </template>
 <script>
@@ -18,18 +45,23 @@ export default {
         xSpreadsheet
     },
     data() {
-        return {};
+        return {
+            xsData: [],
+            renameDialog: false,
+            renameSheetForm: {
+                ind: '',
+                name: ''
+            }
+        };
     },
     mounted() {
+        window.aaa = this;
     },
     methods: {
-        // 监听变化
-        onChange(data) {
-            // console.log(data);
-        },
         // 获取电子表格的方法，监听可以写到里面去
         getSpreadsheet(xs) {
             /**
+             // 监听方法演示
              xs.on('cell-selected', (cell, ri, ci) => {
                 // 监听选择单个单元格
                 console.log('cell:', cell, ', ri:', ri, ', ci:', ci);
@@ -43,9 +75,7 @@ export default {
              */
         },
         getData() {
-            let a = this.$refs.xs.getData();
-            console.log(a)
-            // console.log();
+            return this.$refs.xs.getData();
         },
         // 加载excel文件
         inputChage(fileSelected) {
@@ -54,6 +84,24 @@ export default {
         // 导出excel
         exportExcel() {
             return this.$refs.xs.exportExcel();
+        },
+
+        /** 打开编辑名称的弹窗 */
+        openRenameDialog() {
+            this.xsData = this.getData();
+            this.renameDialog = true;
+        },
+        renameSubmit() {
+            this.xsData[this.renameSheetForm.ind].name = this.renameSheetForm.name;
+            console.log(this.xsData)
+            this.$refs.xs.loadData(this.xsData);
+            this.renameDialog = false;
+        },
+    },
+    watch: {
+        'renameSheetForm.ind'(val, oldVal) {
+            this.renameSheetForm.name = this.xsData[val].name;
+            console.log("renameSheetForm.ind: " + val, oldVal);
         }
     }
 }

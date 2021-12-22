1d4afd2552edea966662a15a5b109b356cdef4a4
Bowen <bowen@jiuchet.com>
2021-12-22 20:44:21 +0800

    开发 进度备份

--- packages/layui/index.js
+++ packages/layui/index.js
@@ -4,3 +4,15 @@ import LayuiCode from './modules/code'
 const layui = [LayuiCode]
 
 export default layui
+
+/*const Layui = require.context('./modules', true, /\.vue$/)
+console.log(321, layui)
+console.log(321, Layui)
+console.log(
+  123,
+  Layui.keys().map((item) => {
+    console.log(item)
+    return layui[item]
+  })
+)*/
+// export default Layui.keys().map(Layui)

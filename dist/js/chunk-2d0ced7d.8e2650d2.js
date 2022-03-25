c0503cc79a9a46372937570810cbe28710d724d6
Bowen <bowen@jiuchet.com>
2022-03-25 23:26:57 +0800

    打包

---
+++ dist/js/chunk-2d0ced7d.8e2650d2.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0ced7d"],{"60d1":function(t,e,n){"use strict";n.r(e);var s=function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"Test"},[t._v("123")])},c=[],a={components:{},data(){return{}},created(){window.VM=this},methods:{}},d=a,o=n("2877"),r=Object(o["a"])(d,s,c,!1,null,null,null);e["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d0ced7d.8e2650d2.js.map

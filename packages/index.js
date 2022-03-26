ba08da7296735c4da5585f954fb8f4d1a16d48fa
Bowen <bowen@jiuchet.com>
2022-03-26 10:41:33 +0800

    自动注册富文本编辑器

--- packages/index.js
+++ packages/index.js
@@ -1,20 +1,19 @@
-// import JcsoftEditor from './editor'
 // import xSpreadsheet from "./xspreadsheet";
 import utils from './utils'
 import layui from './layui'
 import vuetifyKits from './vuetifykits'
 import global from './global/global'
+import JcsoftEditor from './editor'
 
 export function jcsoft() {}
 
 // 存储组件列表（不支持直接注册为组件使用的，不在此处进行注册）
 const components = [
-  /*JcEditor,
-    xSpreadsheet,*/
+  // xSpreadsheet,
   global,
   ...layui,
   ...vuetifyKits,
-  // JcsoftEditor,
+  JcsoftEditor,
 ]
 // 定义 install 方法，接收 Vue 作为参数。如果使用 use 注册插件，则所有的组件都将被注册
 const install = function (Vue) {
@@ -42,10 +41,10 @@ export default {
   // 导出的对象必须具有 install，才能被 Vue.use() 方法安装
   install,
   // 以下是具体的组件列表
-  /*JcEditor,
-      xSpreadsheet,*/
+
+  // xSpreadsheet,
   global,
   ...layui,
   ...vuetifyKits,
-  // JcsoftEditor,
+  JcsoftEditor,
 }
